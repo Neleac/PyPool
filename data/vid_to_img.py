76be1dbe78a20ae0e698a0cd42751a2625c616c1
Caelen <wangc21@uw.edu>
2020-06-04 20:26:39 -0700

    data processing

---
+++ data/vid_to_img.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import os
+import sys
+sys.path.append("../pool_aimbot")
+
+from detect_balls import find_circles
+
+data_dir = "/home/wangc21/datasets/pool"
+cap = cv2.VideoCapture(os.path.join(data_dir, "videos", "4_ball.mp4"))
+fps = 60
+
+idx = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    h, w = frame.shape[0], frame.shape[1]
+    frame = frame[int(0.35 * h) : int(0.65 * h), int(0.25 * w) : int(0.75 * w)]
+    h, w = frame.shape[0], frame.shape[1]
+
+    # find and draw circles
+    circles = find_circles(frame)
+    if circles is not None:
+        """
+        # only draw one ball
+        ball = circles[0, 0]
+        l1, l2 = int(ball[0]) - int(ball[2]), int(ball[1]) - int(ball[2])
+        r1, r2 = int(ball[0]) + int(ball[2]), int(ball[1]) + int(ball[2])
+        l1, l2 = max(0, l1), max(0, l2)
+        r1, r2 = min(w, r1), min(h, r2)
+        '''
+        # save bounding box region
+        bbox = frame[l2 : r2, l1 : r1]
+        bbox = cv2.resize(bbox, (40, 40), interpolation = cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(data_dir, "images", "%d.png" % idx), bbox)
+        idx += 1
+        '''
+        cv2.circle(frame, (ball[0], ball[1]), ball[2], (0, 255, 0), 2)
+        cv2.circle(frame, (ball[0], ball[1]), 2, (0, 0, 255), 3)
+        cv2.rectangle(frame, (l1, l2), (r1, r2), (255, 0, 0), 2)
+        """
+        # draw all balls
+
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+
+    cv2.imshow('frame', frame)
+
+    # playback speed
+    #cv2.waitKey(int(1000 / fps))
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- detect_balls.py
+++ detect_balls.py
@@ -4,19 +4,37 @@ import numpy as np
 # params:
 #   frame - input rgb image
 # returns:
-#   
-def find_circles(frame):
+#   circles - detected circles
+def find_circles(img):
     # smoothing
-    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    frame = cv2.GaussianBlur(img, (5, 5), 0)
 
     # grayscale
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # circle detection
-    dp = 1          # inverse ratio of accumulator resolution to image resolution
-    min_dist = 20   # minimum distance between circle centers
-    param1 = 50     # gradient value for edge detection
-    param2 = 30     # accumulator threshold (increase to get more circles)
-    min_radius = 0  # minimum circle radius
-    max_radius = 0  # maximum circle radius
-    circles = cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, dp, min_dist, param1, param2, min_radius, max_radius)
+    dp = 1              # inverse ratio of accumulator resolution to image resolution
+    min_dist = 30       # minimum distance between circle centers
+    edge_grad = 40      # gradient value for edge detection
+    acc_thresh = 20     # accumulator threshold (increase to get less circles)
+    min_r = 20          # minimum circle radius
+    max_r = 40          # maximum circle radius
+    circles = cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, dp, min_dist, param1=edge_grad, param2=acc_thresh, minRadius=min_r, maxRadius=max_r)
+    
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+    return circles
+
+'''
+frame = cv2.imread("test.png")
+circles = find_circles(frame)
+for i in circles[0, :]:
+    # draw the outer circle
+    cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # draw the center of the circle
+    cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv2.imshow('detected circles', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
